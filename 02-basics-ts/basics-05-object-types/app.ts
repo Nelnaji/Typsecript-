39f81c2b642466106fca70bd4b070577e07c02ef
naofel <nawfal.elkhaznagi@gmail.com>
2024-06-09 16:52:23 +0200

    working on configuration

---
+++ 02-basics-ts/basics-05-object-types/app.ts
@@ -0,0 +1,12 @@
+// const person: {
+//   name: string;
+//   age: number;
+// } = {
+
+// objext types dont have key:value pair, but key:type pair
+const person = {
+  name: 'Maximilian',
+  age: 30
+};
+
+console.log(person.name)
